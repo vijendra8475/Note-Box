5763e9973c59c2a1c78aa7982436faef8baa3942
vijendra <vijjubca@gmail.com>
2025-09-03 13:11:55 +0530

    initial files

--- Frontend/Note_Box/src/main.jsx
+++ Frontend/Note_Box/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'remixicon/fonts/remixicon.css'
+import { Route, Routes } from "react-router"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
   </StrictMode>,
 )
